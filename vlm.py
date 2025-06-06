d2567df9e33e7fbb918bf10603905c8d9c5db565
bigbell999 <bigbell999@knu.ac.kr>
2025-06-06 17:14:38 +0900

    PATCH: tracker wrapping

--- vlm.py
+++ vlm.py
@@ -6,10 +6,13 @@ from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
 class VLM:
   def __init__(self):
+    
     #VLM
     self.model_path = 'OpenGVLab/VideoChat-Flash-Qwen2-7B_res448'
     self.tokenzier = AutoTokenizer.from_pretrained(self.model_path, trust_remote_code=True)
+    #로컬에서 돌리기 위해 잠깐 수정하였음.
     self.model = AutoModel.from_pretrained(self.model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+
     self.image_processor = self.model.get_vision_tower().image_processor
     self.max_num_frames = 512
     self.generation_config = dict(
@@ -25,10 +28,11 @@ class VLM:
     self.translation_tokenizer = MBart50TokenizerFast.from_pretrained(self.translation_model_name)
 
   def vlm_summary(self, angle, video_data):
-    question = "영상은 현재 " + angle + "을 비추고 있는 카메라 영상입니다."
+    question = "영상은 현재 " + angle + "을 비추고 있는 카메라 영상입니다." \
     "서정적인 표현을 제외하고 영상 내용을 간결하게 요약해 주세요. " \
     "자동차 사고 및 사상자가 포함된 경우 상황을 포함하여 요약해 주세요. " \
-    "답변은 영어로 해주세요."
+    "답변은 영어로 해주세요.".format(angle)
+
     output = self.model.chat(video_path=video_data, 
                                             tokenizer=self.tokenizer, 
                                             user_prompt=question, 
@@ -47,7 +51,7 @@ class VLM:
 
     return self.translate(output)
 
-  def convert(image_data):
+  def convert(self, image_data):
     image_files = glob.glob(image_data)
     fps = 24.0
     output_file = 'output_video.mp4'
